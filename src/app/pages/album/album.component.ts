796ad8f6dad0b5c20804cfa81fd3836b6442c86c
Mihkel Baranov <mihkel.baranov@gmail.com>
2020-04-11 17:42:53 +0300

    album view

--- src/app/pages/album/album.component.ts
+++ src/app/pages/album/album.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HttpService } from "src/app/services/http.service";
+import { PlayerService, ITrack } from "src/app/services/player.service";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-album',
-  templateUrl: './album.component.html',
-  styleUrls: ['./album.component.scss']
+	selector: "app-album",
+	templateUrl: "./album.component.html",
+	styleUrls: ["./album.component.scss"],
 })
 export class AlbumComponent implements OnInit {
+	public album: any = {};
+	public tracks: ITrack[] = [];
 
-  constructor() { }
+	constructor(private httpService: HttpService, private playerService: PlayerService, private route: ActivatedRoute) { }
 
-  ngOnInit(): void {
-  }
+	public ngOnInit(): void {
+
+		this.route.params.subscribe((params) => {
+			this.getTracks(params.id);
+			this.getAlbum(params.id);
+		});
+
+	}
+
+	public getTracks(id: string) {
+		this.httpService.get(`/tracks?album=${id}`).subscribe((response: any) => {
+			this.tracks = this.tracks.concat(response.tracks).reverse();
+		}, (err) => {
+			console.log("Failed to load tracks", err);
+		});
+	}
+
+	public onPlayAlbum() {
+		this.playerService.onPlay(...this.tracks);
+	}
+
+	public getAlbum(id: string) {
+		this.httpService.get(`/albums/${id}`).subscribe((response: any) => {
+			this.album = response;
+		}, (err) => {
+			console.log("Failed to load album", err);
+		});
+	}
 
 }
