7517f1f1a76a3f44a4f98ec58c490c310b08565b
Mihkel Baranov <mihkel.baranov@gmail.com>
2020-04-11 14:57:49 +0300

    basic app layout structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { TracksComponent } from "./pages/tracks/tracks.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+
+	{ path: "", redirectTo: "", pathMatch: "full" },
+	{ path: "tracks", component: TracksComponent },
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { PlayerComponent } from "./components/player/player.component";
 import { MenuComponent } from "./components/menu/menu.component";
 import { InfiniteScrollComponent } from "./components/infinite-scroll/infinite-scroll.component";
 import { TrackListComponent } from "./components/track-list/track-list.component";
-import { TracksComponent } from './pages/tracks/tracks.component';
+import { TracksComponent } from "./pages/tracks/tracks.component";
 
 @NgModule({
 	declarations: [
