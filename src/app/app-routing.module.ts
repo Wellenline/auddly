06d63b3ed841ba3462afb90d462d31e5439bb726
Mihkel Baranov <mihkel.baranov@gmail.com>
2020-12-07 08:42:26 +0200

    update @cli to v11

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -40,8 +40,9 @@ const routes: Routes = [
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes, {
-		scrollPositionRestoration: "enabled",
-	})],
+    scrollPositionRestoration: "enabled",
+    relativeLinkResolution: 'legacy'
+})],
 	exports: [RouterModule],
 })
 export class AppRoutingModule { }
