2fa63b2827f73ca6c4beb59b128a2e05a9cc0ace
Mihkel Baranov <mihkel.baranov@gmail.com>
2021-05-27 18:59:05 +0300

    include more by on album view

--- src/app/pages/album/album.component.ts
+++ src/app/pages/album/album.component.ts
@@ -3,6 +3,7 @@ import { HttpService } from "src/app/services/http.service";
 import { PlayerService, ITrack } from "src/app/services/player.service";
 import { ActivatedRoute } from "@angular/router";
 import { InterfaceService } from "src/app/modules/shared/services/interface.service";
+import { SwiperOptions } from "swiper";
 
 @Component({
 	selector: "app-album",
@@ -14,6 +15,39 @@ export class AlbumComponent implements OnInit {
 	public tracks: ITrack[] = [];
 	public duration = 0;
 	public loading = true;
+	public albums = [];
+	public config: SwiperOptions = {
+		slidesOffsetBefore: 20,
+		slidesOffsetAfter: 20,
+		navigation: false,
+		pagination: false,
+		breakpoints: {
+			0: {
+				slidesPerView: 3,
+				spaceBetween: 20
+			},
+			// when window width is >= 320px
+			320: {
+				slidesPerView: 2,
+				spaceBetween: 20
+			},
+			// when window width is >= 480px
+			480: {
+				slidesPerView: 3,
+				spaceBetween: 30
+			},
+			// when window width is >= 640px
+			640: {
+				slidesPerView: 4,
+				spaceBetween: 40
+			},
+			1024: {
+				slidesPerView: 4,
+				spaceBetween: 40
+			},
+
+		},
+	};
 	constructor(private httpService: HttpService,
 		private playerService: PlayerService,
 		private route: ActivatedRoute,
@@ -21,6 +55,7 @@ export class AlbumComponent implements OnInit {
 
 	public ngOnInit(): void {
 		this.route.params.subscribe((params) => {
+			this.albums = [];
 			this.getTracks(params.id);
 			this.getAlbum(params.id);
 		});
@@ -28,7 +63,7 @@ export class AlbumComponent implements OnInit {
 
 	public getTracks(id: string) {
 		this.httpService.get(`/tracks?album=${id}&skip=0&limit=1500`).subscribe((response: { tracks: ITrack[] }) => {
-			this.tracks = this.tracks.concat(response.tracks);
+			this.tracks = response.tracks;
 
 			this.tracks.map((track) => {
 				if (track.duration) {
@@ -56,8 +91,22 @@ export class AlbumComponent implements OnInit {
 		this.loading = true;
 		this.httpService.get(`/albums/${id}`).subscribe((response: any) => {
 			this.album = response;
+
+			this.getAlbums(this.album.artist.id);
+
 		}, (err) => {
 			console.log("Failed to load album", err);
+		}).add(() => {
+			this.loading = false;
+		});
+	}
+
+	public getAlbums(id: string) {
+		this.loading = true;
+		this.httpService.get(`/albums?artist=${id}`).subscribe((response: any) => {
+			this.albums = response.albums.filter((album) => album.id !== this.album.id);
+		}, (err) => {
+			console.log(err);
 		}).add(() => {
 			this.loading = false;
 		});
